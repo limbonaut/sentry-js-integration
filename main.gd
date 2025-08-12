2f4d101f70a51373226844bd13e778f28337322c
Serhii Snitsaruk <limbonaut@pm.me>
2025-08-12 12:13:29 +0200

    Tweak UI

--- main.gd
+++ main.gd
@@ -9,6 +9,9 @@ func _ready():
 	_load_js_script("sentry-godot.js")
 	_load_js_script("sentry-init.js")
 
+	# Test utility functions
+	JavaScriptBridge.eval("helloWorld();")
+
 
 func _add_utility_functions() -> void:
 	JavaScriptBridge.eval("""
@@ -38,14 +41,7 @@ func _load_js_script(p_script: String) -> void:
 	""" % [p_script, p_script], true)
 
 
-func _on_capture_message_btn_pressed() -> void:
-	# Test utility functions
-	JavaScriptBridge.eval("helloWorld();")
-
-	# Get JS Sentry interface
-	SentryJS = JavaScriptBridge.get_interface("Sentry")
-	print("DEBUG: Got interface: ", SentryJS)
-
+func _on_set_context_btn_pressed() -> void:
 	# Set context via custom interface
 	var ctx := {
 		"hello": "world",
@@ -54,9 +50,11 @@ func _on_capture_message_btn_pressed() -> void:
 	var SentryGodot: JavaScriptObject = JavaScriptBridge.get_interface("SentryGodot")
 	SentryGodot.setContext("ctx", JSON.stringify(ctx))
 
+
+func _on_capture_message_btn_pressed() -> void:
+	# Get JS Sentry interface
+	SentryJS = JavaScriptBridge.get_interface("Sentry")
+	print("DEBUG: Got interface: ", SentryJS)
+
 	# Capture message
 	SentryJS.captureMessage("Hello from GDScript")
-
-
-func _on_crash_app_pressed() -> void:
-	OS.crash("Crash button pressed") # doesn't work in WASM
