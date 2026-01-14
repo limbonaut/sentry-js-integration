f12b1c49b49d57e3fd5f5a8e114d5c4485d328b1
Serhii Snitsaruk <limbonaut@pm.me>
2026-01-14 15:28:10 +0100

    Add crash simulation and thread tests

--- main.gd
+++ main.gd
@@ -1,6 +1,5 @@
 extends Node2D
 ## Example of Sentry JS integration in Godot.
-##
 
 
 # Sentry options
@@ -17,6 +16,34 @@ func _ready():
 func _initialize_sentry() -> void:
 	SentryBridge = JavaScriptBridge.get_interface("SentryBridge")
 	SentryBridge.initialize(DSN, RELEASE)
+
+
+func _divide_by_zero() -> void:
+	print("Going to divide some zeroes...")
+	GameLogic.divide(10, 0)
+
+
+func _generate_nonfatal_error(threaded: bool = false) -> void:
+	# Create stacktrace data
+	var stacktrace := {
+		frames = [
+			{
+				filename = "player/damage.gd",
+				function = "_on_area_entered",
+				lineno = 12,
+				in_app = true
+			},
+			{
+				filename = "player/damage.gd",
+				function = "_take_damage",
+				lineno = 5,
+				in_app = true
+			},
+		]
+	}
+
+	var message := "Error test (threaded)" if threaded else "Error test"
+	SentryBridge.captureError(message, JSON.stringify(stacktrace))
 
 
 func _on_set_context_btn_pressed() -> void:
@@ -37,24 +64,24 @@ func _on_capture_message_btn_pressed() -> void:
 
 
 func _on_capture_error_test_pressed() -> void:
-	print("Capturing error...")
+	print("Capturing non-fatal error...")
+	_generate_nonfatal_error()
 
-	# Create stacktrace data
-	var stacktrace := {
-		frames = [
-			{
-				filename = "player/damage.gd",
-				function = "_on_area_entered",
-				lineno = 12,
-				in_app = true
-			},
-			{
-				filename = "player/damage.gd",
-				function = "_take_damage",
-				lineno = 5,
-				in_app = true
-			},
-		]
-	}
 
-	SentryBridge.captureError("Error test", JSON.stringify(stacktrace))
+func _on_capture_error_threaded_test_pressed() -> void:
+	print("Capturing non-fatal error from worker thread...")
+	var thread := Thread.new()
+	thread.start(_generate_nonfatal_error.bind(true))
+	thread.wait_to_finish()
+
+
+func _on_native_division_test_pressed() -> void:
+	print("C++ dividing some zeroes...")
+	_divide_by_zero()
+
+
+func _on_native_division_threaded_test_pressed() -> void:
+	print("C++ dividing some zeroes from worker thread...")
+	var thread := Thread.new()
+	thread.start(_divide_by_zero)
+	thread.wait_to_finish()
